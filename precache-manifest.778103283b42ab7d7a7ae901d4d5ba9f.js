a06fbede823e2e1a0082208c45261a8fcea88891
Boning Li <Boning.li@outlook.com>
2019-04-29 12:36:30 +0200

    Updates

---
+++ precache-manifest.778103283b42ab7d7a7ae901d4d5ba9f.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "143146fa24554ae2c5ac0a3982abb952",
+    "url": "/fonts/ionicons.143146fa.woff2"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "99ac3308dd8ee14f749f51538d0d5b9e",
+    "url": "/fonts/ionicons.99ac3308.woff"
+  },
+  {
+    "revision": "267eb3841286dc7803d9",
+    "url": "/css/chunk-vendors.1dd415b4.css"
+  },
+  {
+    "revision": "00f7b645bea77533a86667362a8541dc",
+    "url": "/img/mock_sol1.00f7b645.jpg"
+  },
+  {
+    "revision": "37353558b124b5ce726f9e88bca2bac9",
+    "url": "/img/404.37353558.png"
+  },
+  {
+    "revision": "e58d00626c3ac0e30bc0aa70d63b1e3e",
+    "url": "/img/mock_sol2.e58d0062.jpg"
+  },
+  {
+    "revision": "c72ad12834f4f78e9a8adaffce753ae3",
+    "url": "/img/mock_qua.c72ad128.jpg"
+  },
+  {
+    "revision": "9d80f29aeff97d3354bb",
+    "url": "/js/app.abb1ec4d.js"
+  },
+  {
+    "revision": "267eb3841286dc7803d9",
+    "url": "/js/chunk-vendors.66176722.js"
+  },
+  {
+    "revision": "d535a25a79fb1365ae814b61e88fae71",
+    "url": "/fonts/ionicons.d535a25a.ttf"
+  },
+  {
+    "revision": "a2c4a261a239aa84463dc70e4bac9b9a",
+    "url": "/img/ionicons.a2c4a261.svg"
+  },
+  {
+    "revision": "9af8aefdc072e73396214c5399b63a55",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9d80f29aeff97d3354bb",
+    "url": "/css/app.39c834d7.css"
+  },
+  {
+    "revision": "9e87eb9c027873e9f96876c7b2f5a973",
+    "url": "/CNAME"
+  },
+  {
+    "revision": "f15b5ec9216aacea294b248110f2c5df",
+    "url": "/404.html"
+  }
+];
