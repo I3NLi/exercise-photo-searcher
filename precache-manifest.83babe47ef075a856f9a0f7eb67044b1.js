09043100ad12eeb1485a2e1b607352ef0d43aece
Boning Li <Boning.li@outlook.com>
2019-04-29 15:59:24 +0200

    Updates

--- precache-manifest.83babe47ef075a856f9a0f7eb67044b1.js
+++ precache-manifest.83babe47ef075a856f9a0f7eb67044b1.js
@@ -1,15 +1,15 @@
 self.__precacheManifest = [
   {
-    "revision": "143146fa24554ae2c5ac0a3982abb952",
-    "url": "/fonts/ionicons.143146fa.woff2"
+    "revision": "99ac3308dd8ee14f749f51538d0d5b9e",
+    "url": "/fonts/ionicons.99ac3308.woff"
   },
   {
     "revision": "735ab4f94fbcd57074377afca324c813",
     "url": "/robots.txt"
   },
   {
-    "revision": "d535a25a79fb1365ae814b61e88fae71",
-    "url": "/fonts/ionicons.d535a25a.ttf"
+    "revision": "143146fa24554ae2c5ac0a3982abb952",
+    "url": "/fonts/ionicons.143146fa.woff2"
   },
   {
     "revision": "33540b99e788e329f69d",
@@ -32,28 +32,28 @@ self.__precacheManifest = [
     "url": "/img/mock_qua.c72ad128.jpg"
   },
   {
-    "revision": "cb5d64d0dcc3cc2456b7",
-    "url": "/js/app.a8bcbf69.js"
+    "revision": "8469e847f1071f95bbdf",
+    "url": "/js/app.cdd069c3.js"
   },
   {
     "revision": "33540b99e788e329f69d",
     "url": "/js/chunk-vendors.79d30348.js"
   },
   {
-    "revision": "99ac3308dd8ee14f749f51538d0d5b9e",
-    "url": "/fonts/ionicons.99ac3308.woff"
+    "revision": "d535a25a79fb1365ae814b61e88fae71",
+    "url": "/fonts/ionicons.d535a25a.ttf"
   },
   {
     "revision": "a2c4a261a239aa84463dc70e4bac9b9a",
     "url": "/img/ionicons.a2c4a261.svg"
   },
   {
-    "revision": "326ccf72dbfff86a8199ff2ae766e80c",
+    "revision": "59e9980f4d57b6785951d45a02af1786",
     "url": "/index.html"
   },
   {
-    "revision": "cb5d64d0dcc3cc2456b7",
-    "url": "/css/app.64baaca3.css"
+    "revision": "8469e847f1071f95bbdf",
+    "url": "/css/app.aeec761b.css"
   },
   {
     "revision": "9e87eb9c027873e9f96876c7b2f5a973",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d79c53d2eec5c82f845155fcbe809947.js"
+  "/precache-manifest.83babe47ef075a856f9a0f7eb67044b1.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "exercise-photo-searcher"});
